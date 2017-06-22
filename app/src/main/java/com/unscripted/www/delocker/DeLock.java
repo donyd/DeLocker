390845ba13788c859ca92c7ce433ee0518808851
= <bonebare@gmail.com>
2017-06-22 21:00:03 +0100

    fixed combination displaying to textview without looper

--- app/src/main/java/com/unscripted/www/delocker/DeLock.java
+++ app/src/main/java/com/unscripted/www/delocker/DeLock.java
@@ -2,6 +2,7 @@ package com.unscripted.www.delocker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,38 +12,57 @@ import java.util.Random;
 
 public class DeLock extends AppCompatActivity {
 
-    private String temp;
-    private char[] currentCombo = new char[3];
-    private String[] combinations;
-
-
+    char[] currentCombo = new char[3];
+    String firstNum, secondNum, thirdNum;
+    char firstChar, secondChar, thirdChar;
+    String temp;
+    String[] combinations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_de_lock);
+
         final Button mBtnGenerate = (Button) findViewById(R.id.btn_generateCombo);
         final TextView mTxtViewONE = (TextView) findViewById(R.id.textView);
         final TextView mTxtViewTWO = (TextView) findViewById(R.id.textView2);
         final TextView mTxtViewTHREE = (TextView) findViewById(R.id.textView3);
+        final TextView mTxtViewWholeDisplay = (TextView) findViewById(R.id.txtWholeDisplay);
 
-        combinations = createRange();
-        temp = combinations[Randomizer()];
-        currentCombo = digitSeparator(temp);
+
+//        firstNum = Integer.toString(currentCombo[0]);
+//        secondNum = Integer.toString(currentCombo[1]);
+//        thirdNum = Integer.toString(currentCombo[2]);
 
         mBtnGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                mTxtViewONE.setText("");
-                mTxtViewTWO.setText("");
-                mTxtViewTHREE.setText("");
+                combinations = createRange();
+                temp = combinations[Randomizer()];
+                currentCombo = digitSeparator(temp);
+
+                firstChar = currentCombo[0];
+                secondChar = currentCombo[1];
+                thirdChar = currentCombo[2];
+
+                firstNum = String.valueOf(firstChar);
+                secondNum = String.valueOf(secondChar);
+                thirdNum = String.valueOf(thirdChar);
+
+
+                mTxtViewONE.setText(firstNum);
+                mTxtViewTWO.setText(secondNum);
+                mTxtViewTHREE.setText(thirdNum);
+
             }
         });
 
+
+
     }
 
-    private String[] createRange(){
+    protected String[] createRange(){
         // Create an array to hold all combination possibilities
         String[] combinations = new String[1000];
 
@@ -61,19 +81,20 @@ public class DeLock extends AppCompatActivity {
         return combinations;
     }
 
-    private int Randomizer(){
+    protected static int Randomizer(){
         Random randomizer = new Random();
         int searchNum =  randomizer.nextInt(1000);
         return searchNum;
     }
 
-    private char[] digitSeparator(String combination){
+    protected static char[] digitSeparator(String combination){
         char[] result = new char[3];
         for(int i = 0; i < combination.length(); i++){
             result[i] = combination.charAt(i);
         }
         return result;
     }
+
 
 
 }
